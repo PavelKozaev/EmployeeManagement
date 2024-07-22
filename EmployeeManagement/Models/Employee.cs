3338f1df1b3fc6475c1ab121543d3fb25f3fdf16
PavelKozaev <pkozaev221299@gmail.com>
2024-07-22 13:31:09 +0300

    Добавление модели данных Employee

---
+++ EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal SalaryPerHour { get; set; }
+    }
+}
